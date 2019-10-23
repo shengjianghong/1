fae66f70973fb10671d4b3d7d76e6b999c9d1da0
xuanpingren <xuanpingren@gmail.com>
2019-10-23 14:52:27 +0800

    初次提交

---
+++ static/quiz.js
@@ -0,0 +1,98 @@
+// Source: https://simplestepscode.com/javascript-quiz-tutorial/
+var myQuestions = [
+    {
+        question:  "<b>When will the UK leave EU?</b>",
+        answers: {
+            a: '31 October 2020.',
+            b: '31 October 2019.',
+            c: 'The article did not say that.',
+            d: 'UK will not leave EU.'
+        },
+        correctAnswer: 'b'
+    }
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
+{
+
+    function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>' + '<br/>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                numCorrect++;
+                // color the answers green
+                answerContainers[i].style.color = 'green';
+            } else {
+                answerContainers[i].style.color = 'red';
+            }
+
+        }
+
+        resultsContainer.innerHTML = '做对' + numCorrect + '题。 共' + questions.length + '题。';
+
+
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+}
